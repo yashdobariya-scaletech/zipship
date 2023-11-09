69596bf08bb1486261af079a2bfce8aab804fc30
vivek898 <vivek.gondaliya@scaletech.xyz>
2023-11-09 16:46:42 +0530

    text change

--- src/features/orderFrom/conatiner/orderForm.tsx
+++ src/features/orderFrom/conatiner/orderForm.tsx
@@ -1,9 +1,9 @@
-import Form from '../component/form';
+import Form from "../component/form";
 
 const OrderForm: React.FC = () => {
   return (
     <>
-      <h1>You can order here and check your status</h1>
+      <h1>You can order here and check your status and check update</h1>
       <Form />
     </>
   );
