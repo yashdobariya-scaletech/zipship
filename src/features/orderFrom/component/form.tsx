19fe168c91eb656fa50b0ce2372085a7d40ba094
vivek898 <vivek.gondaliya@scaletech.xyz>
2023-11-16 17:24:06 +0530

    fields place change

--- src/features/orderFrom/component/form.tsx
+++ src/features/orderFrom/component/form.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import 'react-calendar/dist/Calendar.css';
-import 'react-clock/dist/Clock.css';
-import DateTimePicker from 'react-datetime-picker';
-import 'react-datetime-picker/dist/DateTimePicker.css';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import PhoneInput from 'react-phone-number-input';
-import 'react-phone-number-input/style.css';
-import CollapseBox from '../../../shared/components/collapseBox/collapseBox';
-import ErrorMessage from '../../../shared/components/errorBoundary/errorMessage';
-import { PersonInformation } from '../../../shared/enum/enum';
-import { FormInputField } from './formInputField';
+import { useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import PhoneInput from "react-phone-number-input";
+import DateTimePicker from "react-datetime-picker";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import "react-phone-number-input/style.css";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
+import CollapseBox from "../../../shared/components/collapseBox/collapseBox";
+import ErrorMessage from "../../../shared/components/errorBoundary/errorMessage";
+import { PersonInformation } from "../../../shared/enum/enum";
+import { FormInputField } from "./formInputField";
 
 interface IFormInput {
   senderPlace: string;
@@ -21,7 +21,7 @@ interface IFormInput {
   itemsName: string;
   deliverName: string;
   deliverContactName: string;
-  deliverDateAndTime: Date;
+  // deliverDateAndTime: Date;
   deliverLocation: string;
   deliverEmail: string;
   deliverPhone: string;
@@ -33,67 +33,17 @@ const DataForm: React.FC = () => {
     handleSubmit,
     control,
   } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log("Data", data);
 
-  const [sectionExpand, setSectionExpand] = useState(
-    PersonInformation.SenderInformation as string
-  );
+  const [sectionExpand, setSectionExpand] = useState(PersonInformation.SenderInformation as string);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex--column basic-form container">
-        <h1 className="order-heading font--bold font-size--32 line-height--48 text--center mb--24">
-          Delivery Site
-        </h1>
+        <h1 className="order-heading font--bold font-size--32 line-height--48 text--center mb--24">Delivery Site</h1>
         <div className="sender-wrapper border--grey-500 mb--24 padding--16">
-          <CollapseBox
-            sectionExpand={sectionExpand}
-            setSectionExpand={setSectionExpand}
-            title={'Sender Information'}
-          >
+          <CollapseBox sectionExpand={sectionExpand} setSectionExpand={setSectionExpand} title={"Sender Information"}>
             <div className="form-item mt--5">
-              <FormInputField
-                type="text"
-                title="Place"
-                register={register}
-                name="senderPlace"
-                placeholder="Sender Place"
-              />
-              {errors.senderPlace && (
-                <ErrorMessage name="Sender place is required" />
-              )}
-            </div>
-            <div className="form-item">
-              <FormInputField
-                type="text"
-                title="Contact Name"
-                register={register}
-                name="senderContactName"
-                placeholder="Contact Name"
-              />
-              {errors.senderContactName && (
-                <ErrorMessage name="Contact name is required" />
-              )}
-            </div>
-            <div className="form-item">
-              <p className="mb--5 font--bold">Email</p>
-              <input
-                type="email"
-                className="form__input"
-                placeholder="Sender Email"
-                {...register('senderEmail', {
-                  required: 'Sender email is required',
-                  pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: 'invalid email address',
-                  },
-                })}
-              />
-              {errors.senderEmail?.message && (
-                <ErrorMessage name={errors.senderEmail?.message} />
-              )}
-            </div>
-            <div className="form-item">
               <Controller
                 name="senderDateAndTime"
                 control={control}
@@ -101,79 +51,57 @@ const DataForm: React.FC = () => {
                 render={({ field }) => (
                   <>
                     <p className="mb--5 font--bold">Date And Time</p>
-                    <DateTimePicker className={'width--full'} {...field} />
+                    <DateTimePicker className={"width--full"} {...field} />
                   </>
                 )}
               />
-              {errors.senderDateAndTime && (
-                <ErrorMessage name={'Please add date and time'} />
-              )}
+              {errors.senderDateAndTime && <ErrorMessage name={"Please add date and time"} />}
+            </div>
+            <div className="form-item">
+              <FormInputField type="text" title="Place" register={register} name="senderPlace" placeholder="Sender Place" />
+              {errors.senderPlace && <ErrorMessage name="Sender place is required" />}
+            </div>
+            <div className="form-item">
+              <FormInputField type="text" title="Contact Name" register={register} name="senderContactName" placeholder="Contact Name" />
+              {errors.senderContactName && <ErrorMessage name="Contact name is required" />}
+            </div>
+            <div className="form-item">
+              <p className="mb--5 font--bold">Email</p>
+              <input
+                type="email"
+                className="form__input"
+                placeholder="Sender Email"
+                {...register("senderEmail", {
+                  required: "Sender email is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "invalid email address",
+                  },
+                })}
+              />
+              {errors.senderEmail?.message && <ErrorMessage name={errors.senderEmail?.message} />}
             </div>
             <div className="form-item">
               <p className="mb--5 font--bold">Phone Number</p>
-              <Controller
-                name="senderPhone"
-                control={control}
-                rules={{ required: true }}
-                render={({ field: { onChange, value } }) => (
-                  <PhoneInput
-                    className="form__input"
-                    defaultCountry="US"
-                    specialLabel={''}
-                    international
-                    onChange={onChange}
-                    value={value}
-                  />
-                )}
-              />
-              {errors.senderPhone && (
-                <ErrorMessage name="Phone number is required" />
-              )}
+              <Controller name="senderPhone" control={control} rules={{ required: true }} render={({ field: { onChange, value } }) => <PhoneInput className="form__input" defaultCountry="US" international onChange={onChange} value={value} />} />
+              {errors.senderPhone && <ErrorMessage name="Phone number is required" />}
             </div>
 
             <div className="form-item">
-              <FormInputField
-                type="text"
-                title="Items Name"
-                register={register}
-                name="itemsName"
-                placeholder="Items Name"
-              />
-              {errors.itemsName && (
-                <ErrorMessage name="Items name is required" />
-              )}
+              <FormInputField type="text" title="Items Name" register={register} name="itemsName" placeholder="Items Name" />
+              {errors.itemsName && <ErrorMessage name="Items name is required" />}
             </div>
           </CollapseBox>
         </div>
         <div className="deliver-wrapper border--grey-500 padding--16 mb--24">
-          <CollapseBox
-            sectionExpand={sectionExpand}
-            setSectionExpand={setSectionExpand}
-            title={'Deliver Information'}
-          >
+          <CollapseBox sectionExpand={sectionExpand} setSectionExpand={setSectionExpand} title={"Delivery Information"}>
             <div className="form-item mt--5">
-              <FormInputField
-                type="text"
-                title="Name"
-                register={register}
-                name="deliverName"
-                placeholder="Deliver Name"
-              />
-              {errors.deliverName && (
-                <ErrorMessage name="Deliver name is required" />
-              )}
+              <FormInputField type="text" title="Place" register={register} name="deliverName" placeholder="Deliver To" />
+              {errors.deliverName && <ErrorMessage name="Deliver name is required" />}
             </div>
             <div className="form-item">
-              <FormInputField
-                type="text"
-                title="Contact Name"
-                register={register}
-                name="deliverContactName"
-                placeholder="Contact Name"
-              />
-              {errors.deliverContactName && (
-                <ErrorMessage name="Contact name is required" />
-              )}
+              <FormInputField type="text" title="Contact Name" register={register} name="deliverContactName" placeholder="Contact Name" />
+              {errors.deliverContactName && <ErrorMessage name="Contact name is required" />}
             </div>
             <div className="form-item">
               <p className="mb--5 font--bold">Email</p>
@@ -181,19 +109,17 @@ const DataForm: React.FC = () => {
                 type="email"
                 className="form__input"
                 placeholder="Deliver Email"
-                {...register('deliverEmail', {
-                  required: 'Deliver email is required',
+                {...register("deliverEmail", {
+                  required: "Deliver email is required",
                   pattern: {
                     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: 'invalid email address',
+                    message: "invalid email address",
                   },
                 })}
               />
-              {errors.deliverEmail?.message && (
-                <ErrorMessage name={errors.deliverEmail?.message} />
-              )}
+              {errors.deliverEmail?.message && <ErrorMessage name={errors.deliverEmail?.message} />}
             </div>
-            <div className="form-item">
+            {/* <div className="form-item">
               <Controller
                 name="deliverDateAndTime"
                 control={control}
@@ -201,52 +127,26 @@ const DataForm: React.FC = () => {
                 render={({ field }) => (
                   <>
                     <p className="mb--5 font--bold">Date And Time</p>
-                    <DateTimePicker className={'width--full'} {...field} />
+                    <DateTimePicker className={"width--full"} {...field} />
                   </>
                 )}
               />
-              {errors.deliverDateAndTime && (
-                <ErrorMessage name={'Please add date and time'} />
-              )}
-            </div>
+              {errors.deliverDateAndTime && <ErrorMessage name={"Please add date and time"} />}
+            </div> */}
             <div className="form-item">
               <p className="mb--5 font--bold">Phone Number</p>
-              <Controller
-                name="deliverPhone"
-                control={control}
-                rules={{ required: true }}
-                render={({ field: { onChange, value } }) => (
-                  <PhoneInput
-                    className="form__input"
-                    defaultCountry="US"
-                    specialLabel={''}
-                    international
-                    onChange={onChange}
-                    value={value}
-                  />
-                )}
-              />
-              {errors.deliverPhone && (
-                <ErrorMessage name="Phone number is required" />
-              )}
+              <Controller name="deliverPhone" control={control} rules={{ required: true }} render={({ field: { onChange, value } }) => <PhoneInput className="form__input" defaultCountry="US" international onChange={onChange} value={value} />} />
+              {errors.deliverPhone && <ErrorMessage name="Phone number is required" />}
             </div>
             <div className="form-item">
-              <FormInputField
-                type="text"
-                title="Location"
-                register={register}
-                name="deliverLocation"
-                placeholder="Deliver Location"
-              />
-              {errors.deliverLocation && (
-                <ErrorMessage name="Location is required" />
-              )}
+              <FormInputField type="text" title="Location" register={register} name="deliverLocation" placeholder="Deliver Location" />
+              {errors.deliverLocation && <ErrorMessage name="Location is required" />}
             </div>
           </CollapseBox>
         </div>
         <div className="form-item">
           <button className="form__submit font--bold text--white" type="submit">
-            Send
+            Submit
           </button>
         </div>
       </div>
